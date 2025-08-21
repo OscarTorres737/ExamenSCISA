e3ba838ca5a767008efda1edf730314e7e1d2d92
Oscar Ariel Torres Martínez <99297896+OscarTorres737@users.noreply.github.com>
2025-08-20 23:58:47 -0600

    Add files via upload

---
+++ ExamenSCISA.Presentation/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ExamenSCISA.Presentation
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ExamenSCISA.Presentation/Controllers/PokemonController.cs
@@ -0,0 +1,28 @@
+﻿using ExamenSCISA.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamenSCISA.Presentation.Controllers
+{
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonService _pokemonServicio;
+
+        public PokemonController(IPokemonService pokemonServicio)
+        {
+            _pokemonServicio = pokemonServicio;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var lista = await _pokemonServicio.ObtenerPokemones(0, 20); // primeros 20 regsitros
+                return View(lista); // pasamos la data a la vista
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("hubo un error al traer pokemones: " + ex.Message);
+            }
+        }
+    }
+}
